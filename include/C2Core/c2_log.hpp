fa3102b80a7f505052039026ea19ebe08444bec7
con2222 <con200956@gmail.com>
2026-07-27 12:50:41 +0300

    Add logs function

---
+++ include/C2Core/c2_log.hpp
@@ -0,0 +1,24 @@
+#ifndef C2_LOG_HPP
+#define C2_LOG_HPP
+
+#include <string_view>
+
+
+namespace C2Core::Log {
+
+
+void Info(std::string_view message);
+void Info(const char* format, ...);
+
+void Warning(std::string_view message);
+void Warning(const char* format, ...);
+
+void Error(std::string_view message);
+void Error(const char* format, ...);
+
+void ErrorDetailed(std::string_view message, const char* file, int line);
+
+
+} // C2Core::Log
+
+#endif // C2_LOG_HPP
